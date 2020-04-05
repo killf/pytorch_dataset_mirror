5372e450202cc39b97900d534aefb55da48b7ecd
killf <killf@foxmail.com>
2020-04-05 23:34:38 +0800

    update

--- torch_dataset_mirror/__init__.py
+++ torch_dataset_mirror/__init__.py
@@ -47,6 +47,50 @@ try:
         'nist': [(join(DATA_PATH, 'xnist-images-idx3-ubyte.xz'), '7f124b3b8ab81486c9d8c2749c17f834'),
                  (join(DATA_PATH, 'xnist-labels-idx2-int.xz'), '5ed0e788978e45d4a8bd4b7caec3d79d')]
     }
+
+    DATA_PATH = join(BASE_PATH, 'torchvision', 'cifar')
+    torchvision.datasets.cifar.CIFAR10.url = join(DATA_PATH, 'cifar-10-python.tar.gz'),
+    torchvision.datasets.cifar.CIFAR100.url = join(DATA_PATH, 'cifar-100-python.tar.gz'),
+
+    DATA_PATH = join(BASE_PATH, 'torchvision', 'pascal')
+    DATASET_YEAR_DICT = {
+        '2012': {
+            'url': join(DATA_PATH, 'VOCtrainval_11-May-2012.tar'),
+            'filename': 'VOCtrainval_11-May-2012.tar',
+            'md5': '6cd6e144f989b92b3379bac3b3de84fd',
+            'base_dir': os.path.join('VOCdevkit', 'VOC2012')
+        },
+        '2011': {
+            'url': join(DATA_PATH, 'http://host.robots.ox.ac.uk/pascal/VOC/voc2011/VOCtrainval_25-May-2011.tar'),
+            'filename': 'VOCtrainval_25-May-2011.tar',
+            'md5': '6c3384ef61512963050cb5d687e5bf1e',
+            'base_dir': os.path.join('TrainVal', 'VOCdevkit', 'VOC2011')
+        },
+        '2010': {
+            'url': join(DATA_PATH, 'VOCtrainval_03-May-2010.tar'),
+            'filename': 'VOCtrainval_03-May-2010.tar',
+            'md5': 'da459979d0c395079b5c75ee67908abb',
+            'base_dir': os.path.join('VOCdevkit', 'VOC2010')
+        },
+        '2009': {
+            'url': join(DATA_PATH, 'VOCtrainval_11-May-2009.tar'),
+            'filename': 'VOCtrainval_11-May-2009.tar',
+            'md5': '59065e4b188729180974ef6572f6a212',
+            'base_dir': os.path.join('VOCdevkit', 'VOC2009')
+        },
+        '2008': {
+            'url': join(DATA_PATH, 'VOCtrainval_14-Jul-2008.tar'),
+            'filename': 'VOCtrainval_11-May-2012.tar',
+            'md5': '2629fa636546599198acfcfbfcf1904a',
+            'base_dir': os.path.join('VOCdevkit', 'VOC2008')
+        },
+        '2007': {
+            'url': join(DATA_PATH, 'VOCtrainval_06-Nov-2007.tar'),
+            'filename': 'VOCtrainval_06-Nov-2007.tar',
+            'md5': 'c52e279531787c972589f7e41ab4ae64',
+            'base_dir': os.path.join('VOCdevkit', 'VOC2007')
+        }
+    }
 except ImportError:
     pass
 
