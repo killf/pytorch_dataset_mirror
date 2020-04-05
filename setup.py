406bd34ea70c6ac77fdb9d80947ee8bea0dbe06b
killf <killf@foxmail.com>
2020-04-05 18:56:57 +0800

    update

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
 setuptools.setup(
     name="torch_dataset_mirror",
     version="0.1.0",
+    license="Apache License",
     author="killf",
     author_email="killf@foxmail.com",
     description="pytorch数据集国内镜像",
@@ -21,9 +23,6 @@ setuptools.setup(
         'Development Status :: 5 - Production/Stable',
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: Apache License',
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
